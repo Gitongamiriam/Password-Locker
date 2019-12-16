d45d9c7949b84e19ca7b87c4883dac1c92bf7ef0
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-12-16 09:54:37 +0300

    Add function for creating a new user account

--- password.py
+++ password.py
@@ -1,5 +1,12 @@
-class User_Credentials:
-    
- def create_user()   
-    
-        
+import pyperclip
+from user_credentials import User, Credential
+
+def create_user(fname,password):
+	'''
+	Function to create an account
+	'''
+	new_user = User(fname,password)
+	return new_user
+
+
+
